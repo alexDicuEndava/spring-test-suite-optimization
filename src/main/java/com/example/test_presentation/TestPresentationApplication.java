0088c8da2ca9af136a445b65f1616d19ddacb003
Alexandru Dicu <alexandru.dicu@endava.com>
2026-06-19 13:12:12 +0300

    Initial commit

---
+++ src/main/java/com/example/test_presentation/TestPresentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_presentation/TestPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
